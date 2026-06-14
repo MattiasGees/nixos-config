3c93300053a408bcda8d95fc45d77fd314d59c0e
Mattias Gees <mattias@gees.dev>
2026-06-14 19:58:19 +0100

    Add flux

--- pkgs/kube.nix
+++ pkgs/kube.nix
@@ -4,7 +4,7 @@ with pkgs;
   home = {
     packages = with pkgs; [
       # all the kubernetes related stuff
-      kubectx kubectl awscli2 (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) kustomize ansible
+      kubectx kubectl awscli2 (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) kustomize ansible flux
       helmfile kubernetes-helm k9s crane kind awscli2 cosign istioctl k3d
       # argo-workflows argocd kubectx kubectl awscli2 (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) kustomize
       # helmfile kubernetes-helm k9s crane minikube kind awscli2 cosign syft grype ko cmctl dive tilt stern skaffold
